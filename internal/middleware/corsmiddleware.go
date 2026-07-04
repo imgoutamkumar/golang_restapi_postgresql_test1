97942035638c864e7233d9618119503574943516
Goutam Kumar <goutamkumar42010no@gmail.com>
2026-07-04 17:25:48 +0530

    add new cors link

--- internal/middleware/corsmiddleware.go
+++ internal/middleware/corsmiddleware.go
@@ -5,11 +5,12 @@ import "github.com/gin-gonic/gin"
 func CORSMiddleware() gin.HandlerFunc {
 
 	allowedOrigins := map[string]bool{
-		"http://localhost:5173":                                                 true, // dev
-		"https://your-frontend.com":                                             true, // prod
-		"https://www.your-frontend.com":                                         true,
-		"https://fashion-gules-pi.vercel.app":                                   true,
+		"http://localhost:5173":                                                true, // dev
+		"https://your-frontend.com":                                            true, // prod
+		"https://www.your-frontend.com":                                        true,
+		"https://fashion-gules-pi.vercel.app":                                  true,
 		"https://myshop-frontend-ecqs2yqgt-imgoutamkumars-projects.vercel.app": true,
+		"https://myshop-frontend-kappa.vercel.app":                             true,
 	}
 
 	return func(c *gin.Context) {
